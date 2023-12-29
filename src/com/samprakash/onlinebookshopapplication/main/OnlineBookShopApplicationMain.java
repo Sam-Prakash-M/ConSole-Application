0c90efd650fa015f7349377d1431ecfc9ab70546
msamp <msamp@Sam>
2023-12-29 21:15:34 +0530

    Completed Console

--- src/com/samprakash/onlinebookshopapplication/main/OnlineBookShopApplicationMain.java
+++ src/com/samprakash/onlinebookshopapplication/main/OnlineBookShopApplicationMain.java
@@ -4,7 +4,7 @@ import com.samprakash.onlinebookshopapplication.view.jsondata.JsonDataview;
 
 public class OnlineBookShopApplicationMain {
    
-	 
+	 //Start of Process
 	public static void main(String[] args) {
 		 JsonDataview jsonDataView = new JsonDataview();
 		jsonDataView.startTheApp();
